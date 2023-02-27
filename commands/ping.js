2ab3f6cc4f8260a24ae473c44e43747262c55122
Liam <ericksonl@sou.edu>
2023-02-27 04:10:47 -0800

    working on commands, changed schema

--- commands/ping.js
+++ commands/ping.js
@@ -1,5 +1,6 @@
 const { SlashCommandBuilder, EmbedBuilder } = require('discord.js')
 const setupSchema = require('../mongooseSchema/schema.js')
+
 const CryptoJS = require("crypto-js");
 
 module.exports = {
@@ -10,12 +11,13 @@ module.exports = {
   async execute(interaction) {
     const user = interaction.user.id
 
+    interaction.reply("huh")
     var encrypted
 
     setupSchema.findOne({ UserID: user }, async (err, data) => {
       if (data) {    
         encrypted = data.Key
-        
+
         console.log(encrypted)
 
 
--- mongooseSchema/schema.js
+++ mongooseSchema/schema.js
@@ -1,9 +1,9 @@
-const { model, Schema }  = require('mongoose')
+const { model, Schema } = require('mongoose')
 
 const setupSchema = new Schema({
     UserID: String,
     Key: String,
-    DailyJournal: Array
-})
+    DailyJournal: Object
+}, { minimize: false })
 
 module.exports = model('journal-entries', setupSchema, 'journal-entries')
